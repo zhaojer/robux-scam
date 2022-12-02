8441d132cbb366042dfce5f6aa35eee57eb64acb
wbguo <wbguo@umich.edu>
2022-12-01 20:41:49 -0500

    updated login, no popover still :(

--- robux-scam/js/login-edu.js
+++ robux-scam/js/login-edu.js
@@ -5,18 +5,69 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      popoverList: [],
+      toastList: [],
+
+      completePopup: {},
+
+      // track the number of clicks for each popover
+      numLogoClick: 0,
+      numTitleClick: 0,
+      numButtonClick: 0
     }
   },
   methods: {
-    openAgeRestrictionModal: function () {
-      let ageRestriction = new bootstrap.Modal(document.getElementById("age-restriction"));
-      ageRestriction.show();
+    checkCompletion: function () {
+      if (this.numLogoClick >= 2 && this.numTitleClick >= 2 && this.numButtonClick >= 2) {
+        console.log("done");
+        this.completePopup = new bootstrap.Modal(document.getElementById("proceed"));
+        this.completePopup.show();
+        return;
+      }
+      let count = 0;
+      if (this.numLogoClick >= 2) {
+        ++count;
+      }
+      if (this.numTitleClick >= 2) {
+        ++count;
+      }
+      if (this.numButtonClick >= 2) {
+        ++count;
+      }
+      this.toastList[count - 1].show();
     },
-  }
-}).mount('#landing-page')
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
+    logoClick: function () {
+      this.numLogoClick += 1;
+      if (this.numLogoClick % 2 === 0) {
+        setTimeout(this.checkCompletion, 500);
+      }
+    },
+
+    titleClick: function () {
+      this.numTitleClick += 1;
+      if (this.numTitleClick % 2 === 0) {
+        setTimeout(this.checkCompletion, 500);
+      }
+    },
+
+    buttonClick: function () {
+      this.numButtonClick += 1;
+      if (this.numButtonClick % 2 === 0) {
+        setTimeout(this.checkCompletion, 500);
+      }
+    },
+  },
+  
+  mounted() {
+  const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  this.popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+  const toastElList = [].slice.call(document.querySelectorAll('.toast'))
+  this.toastList = toastElList.map(function (toastEl) {
+    return new bootstrap.Toast(toastEl)
+  })
+}
+}).mount('#login-edu')
